c6350f43d6dab0e32271c57eb802b1401acfbc69
Eduardo Sebastian <esebastian@altiora.es>
2019-12-02 17:06:32 +0100

    Add 2020 budget data
    There are substantial changes in the institutional classification codes,
    so we've had to configure a new institutional mapping to make codes
    consistent across years.
    
    A full data reload would be needed to ensure the mapping is applied.
    
    civio/presupuesto-management#963

--- loaders/madrid_budget_loader.py
+++ loaders/madrid_budget_loader.py
@@ -12,7 +12,7 @@ class MadridBudgetLoader(SimpleBudgetLoader):
     def parse_item(self, filename, line):
         # Programme codes have changed in 2015, due to new laws. Since the application expects a code-programme
         # mapping to be constant over time, we are forced to amend budget data prior to 2015.
-        programme_mapping = {
+        programme_mapping_pre_2015 = {
             # old programme: new programme
             '13304': '13402',   # Planificación de la movilidad
             '13305': '13403',   # Instalaciones de gestión del tráfico
@@ -96,7 +96,7 @@ class MadridBudgetLoader(SimpleBudgetLoader):
         # The institutional structure of the City of Madrid has changed quite a lot along the
         # years, es In order to show the evolution of a given section we need to keep codes
         # consistent.
-        institutional_mapping = {
+        institutional_mapping_2015 = {
             '0085': '0027',     # EQUIDAD, DERECHOS SOCIALES Y EMPLEO
             '0033': '0037',     # COORDINACIÓN TERRITORIAL Y ASOCIACIONES
             '0041': '0047',     # PORTAVOZ, COORD. JUNTA GOB. Y RELAC. CON EL PLENO
@@ -109,7 +109,34 @@ class MadridBudgetLoader(SimpleBudgetLoader):
         }
 
         institutional_mapping_pre_2019 = {
+            '0011': '0020',     # Vicealcaldía
             '0075': '007A',     # Área de Economía, Empleo y Participación Ciudadana
+        }
+
+        institutional_mapping_pre_2020 = {
+            '0002': '0100',     # Presidencia del Pleno
+            '0003': '0103',     # Oficina Municipal contra el Fraude y la Corrupción
+            '0010': '0101',     # Alcaldía
+            '0012': '0102',     # Coordinación General de la Alcaldía
+            '0020': '0110',     # Vicealcaldía
+            '0021': '0111',     # Área delegada de Coordinación Territorial, Transparencia y Participación Ciudadana
+            '0023': '0112',     # Área delegada de Internacionalización y Cooperación
+            '0027': '0180',     # Familias, Igualdad y Bienestar Social
+            '0031': '0161',     # Área Delegada de Vivienda
+            '0055': '0190',     # Obras y Equipamientos
+            '0057': '0170',     # Hacienda y Personal
+            '0060': '0140',     # Economía, Innovación y Empleo
+            '0065': '0131',     # Área Delegada de Deporte
+            '0066': '0132',     # Área Delegada de Turismo
+            '0067': '0120',     # Portavoz, Seguridad y Emergencias
+            '0075': '0141',     # Área Delegada de Emprendimiento, Empleo e Innovación
+            '0087': '0160',     # Desarrollo Urbano
+            '0097': '0150',     # Medio Ambiente y Movilidad
+            '0098': '0130',     # Cultura, Turismo y Deporte
+            '0100': '0300',     # Endeudamiento
+            '0110': '0310',     # Créditos Globales y Fondo de Contingencia
+            '0120': '0320',     # Tribunal Económico-Administrativo
+            '0130': '013A',     # Defensor del Contribuyente
         }
 
         # Skip first line
@@ -159,10 +186,13 @@ class MadridBudgetLoader(SimpleBudgetLoader):
 
             # Apply institutional mapping to make codes consistent across years
             if int(year) <= 2015:
-                ic_code = institutional_mapping.get(ic_code, ic_code)
+                ic_code = institutional_mapping_2015.get(ic_code, ic_code)
 
             if int(year) < 2019:
                 ic_code = institutional_mapping_pre_2019.get(ic_code, ic_code)
+
+            if int(year) < 2020:
+                ic_code = institutional_mapping_pre_2020.get(ic_code, ic_code)
 
             # Some years require some amendments
             if year == '2011':
@@ -176,7 +206,7 @@ class MadridBudgetLoader(SimpleBudgetLoader):
 
             # For years before 2015 we check whether we need to amend the programme code
             if int(year) < 2015:
-                fc_code = programme_mapping.get(fc_code, fc_code)
+                fc_code = programme_mapping_pre_2015.get(fc_code, fc_code)
 
             # For years before 2019 we check whether we need to amend the programme code
             if int(year) < 2019:
--- loaders/madrid_payments_loader.py
+++ loaders/madrid_payments_loader.py
@@ -1,10 +1,9 @@
 # -*- coding: UTF-8 -*-
 
-import datetime
-import re
-
 from budget_app.loaders import PaymentsLoader
 from budget_app.models import Budget
+
+import re
 
 
 class MadridPaymentsLoader(PaymentsLoader):
@@ -13,7 +12,7 @@ class MadridPaymentsLoader(PaymentsLoader):
         # The institutional structure of the City of Madrid has changed quite a lot along the
         # years, es In order to show the evolution of a given section we need to keep codes
         # consistent.
-        institutional_mapping = {
+        institutional_mapping_2015 = {
             '0085': '0027',  # EQUIDAD, DERECHOS SOCIALES Y EMPLEO
             '0033': '0037',  # COORDINACIÓN TERRITORIAL Y ASOCIACIONES
             '0041': '0047',  # PORTAVOZ, COORD. JUNTA GOB. Y RELAC. CON EL PLENO
@@ -23,6 +22,37 @@ class MadridPaymentsLoader(PaymentsLoader):
             '0035': '0087',  # DESARROLLO URBANO SOSTENIBLE
             '0015': '0097',  # MEDIO AMBIENTE Y MOVILIDAD
             '0065': '0098',  # CULTURA Y DEPORTES
+        }
+
+        institutional_mapping_pre_2019 = {
+            '0011': '0020',     # Vicealcaldía
+            '0075': '007A',     # Área de Economía, Empleo y Participación Ciudadana
+        }
+
+        institutional_mapping_pre_2020 = {
+            '0002': '0100',     # Presidencia del Pleno
+            '0003': '0103',     # Oficina Municipal contra el Fraude y la Corrupción
+            '0010': '0101',     # Alcaldía
+            '0012': '0102',     # Coordinación General de la Alcaldía
+            '0020': '0110',     # Vicealcaldía
+            '0021': '0111',     # Área delegada de Coordinación Territorial, Transparencia y Participación Ciudadana
+            '0023': '0112',     # Área delegada de Internacionalización y Cooperación
+            '0027': '0180',     # Familias, Igualdad y Bienestar Social
+            '0031': '0161',     # Área Delegada de Vivienda
+            '0055': '0190',     # Obras y Equipamientos
+            '0057': '0170',     # Hacienda y Personal
+            '0060': '0140',     # Economía, Innovación y Empleo
+            '0065': '0131',     # Área Delegada de Deporte
+            '0066': '0132',     # Área Delegada de Turismo
+            '0067': '0120',     # Portavoz, Seguridad y Emergencias
+            '0075': '0141',     # Área Delegada de Emprendimiento, Empleo e Innovación
+            '0087': '0160',     # Desarrollo Urbano
+            '0097': '0150',     # Medio Ambiente y Movilidad
+            '0098': '0130',     # Cultura, Turismo y Deporte
+            '0100': '0300',     # Endeudamiento
+            '0110': '0310',     # Créditos Globales y Fondo de Contingencia
+            '0120': '0320',     # Tribunal Económico-Administrativo
+            '0130': '013A',     # Defensor del Contribuyente
         }
 
         # But what we want as area is the programme description
@@ -58,7 +88,13 @@ class MadridPaymentsLoader(PaymentsLoader):
 
         # Apply institutional mapping to make codes consistent across years
         if budget.year <= 2015:
-            ic_code = institutional_mapping.get(ic_code, ic_code)
+            ic_code = institutional_mapping_2015.get(ic_code, ic_code)
+
+        if budget.year < 2019:
+            ic_code = institutional_mapping_pre_2019.get(ic_code, ic_code)
+
+        if budget.year < 2020:
+            ic_code = institutional_mapping_pre_2020.get(ic_code, ic_code)
 
         return {
             'area': policy,
