7cbb2e03d84b34e649872c2217a89cd45a15134e
David Cabo <david.cabo@gmail.com>
2022-11-26 03:17:40 +0100

    Load monitoring goals into database civio/presupuesto-management#1216

--- loaders/madrid_monitoring_loader.py
+++ loaders/madrid_monitoring_loader.py
@@ -6,4 +6,18 @@ import re
 
 class MadridMonitoringLoader(MonitoringLoader):
 
-    None
+    def parse_goal(self, filename, line):
+        # Skip empty/header/subtotal lines.
+        if line[0]=='' or line[0]=='Entidad CP':
+            return
+
+        # Get institutional code, which requires some manipulation. We ignore sections in autonomous bodies.
+        institution_code = '0' if line[4][0:3] != '001' else line[4][2]
+        department_code = line[4][3:6] if institution_code == '0' else '00'
+
+        return {
+            'ic_code': institution_code + department_code,
+            'fc_code': line[5],
+            'goal_number': line[6],
+            'description': line[7]
+        }
