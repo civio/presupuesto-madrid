a20972ebcb9e5b0166edd8dc643c5640256b9b50
David Cabo <david.cabo@gmail.com>
2023-02-26 18:46:49 +0100

    If no monitoring data is available, store NULL, not zeroes civio/presupuesto-management#1203

--- loaders/madrid_monitoring_loader.py
+++ loaders/madrid_monitoring_loader.py
@@ -62,25 +62,32 @@ class MadridMonitoringLoader(MonitoringLoader):
         description = line[4].decode("utf8")
         unit = line[5]
         target = int(line[6])
-        actual = int(line[7])
         _is_inverse_indicator = self._is_inverse_indicator(description, unit)
 
+        # Do we actually have data? We've modified the admin panel to remove the last column
+        # when we know there's no data. In the original files there are zeroes, which is
+        # very confusing.
+        actual = int(line[7]) if len(line)>7 else None
+
         # Calculate the indicator score, from 0 to 1
-        if _is_inverse_indicator:
-            # You get 0 points for doubling the target, and better from that.
-            # Note that you could be worse than that (while "normal" indicators don't
-            # go below zero), so we need to cap both the minimum and the maximum.
-            # Note: If the goal is 0 (sometimes the data is flawed) then the score
-            # is zero. We can't calculate it anyway. It's rare, but it happens.
-            score = 0 if target==0 else max(min(float(2*target-actual)/float(target), 1.0), 0.0)
+        if actual==None:
+            score = None
         else:
-            if target==0:
-                # This is probably a mistake, but it happens sometimes. We've been
-                # asked explicitely (#1203) to "grant" the score.
-                score = 1.0 if (float(actual)>0) else 0.0
+            if _is_inverse_indicator:
+                # You get 0 points for doubling the target, and better from that.
+                # Note that you could be worse than that (while "normal" indicators don't
+                # go below zero), so we need to cap both the minimum and the maximum.
+                # Note: If the goal is 0 (sometimes the data is flawed) then the score
+                # is zero. We can't calculate it anyway. It's rare, but it happens.
+                score = 0 if target==0 else max(min(float(2*target-actual)/float(target), 1.0), 0.0)
             else:
-                # Note: we assume negative values do not exist, simpler this way.
-                score = min(float(actual)/float(target), 1.0)
+                if target==0:
+                    # This is probably a mistake, but it happens sometimes. We've been
+                    # asked explicitely (#1203) to "grant" the score.
+                    score = 1.0 if (float(actual)>0) else 0.0
+                else:
+                    # Note: we assume negative values do not exist, simpler this way.
+                    score = min(float(actual)/float(target), 1.0)
 
         return {
             'goal_uid': self._get_goal_uid(year, ic_code, fc_code, goal_number),
