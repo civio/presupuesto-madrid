88bb11101bd3d3e59c2b718f156da13399d7451a
David Cabo <david.cabo@gmail.com>
2025-03-20 22:23:08 +0100

    Use Python 3-compatible imports civio/presupuesto-management#1286

--- views/__init__.py
+++ views/__init__.py
@@ -1,3 +1,10 @@
-from guidedvisit import guidedvisit
-from csv_xls import inflation_stats, population_stats
-from admin import *
+import six
+
+if six.PY2:
+    from guidedvisit import guidedvisit
+    from csv_xls import inflation_stats, population_stats
+    from admin import *
+else:
+    from .guidedvisit import guidedvisit
+    from .csv_xls import inflation_stats, population_stats
+    from .admin import *
